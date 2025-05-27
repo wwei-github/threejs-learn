15558dffed152d91123d7edac0b2dc25132175a8
zane <zanesdf@qq.com>
2025-05-27 23:29:33 +0800

    第二课: 平移、旋转、缩放

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { color } from "three/tsl";
 
 const Canvas = document.querySelector("canvas.webgl");
 
@@ -8,10 +9,39 @@ const size = {
   height: 600,
 };
 
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
+
+const group = new THREE.Group();
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const mesh = new THREE.Mesh(boxGeometry, material);
-scene.add(mesh);
+group.add(mesh);
+
+const boxGeometry1 = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial1 = new THREE.MeshBasicMaterial({ color: "red" });
+const boxMesh1 = new THREE.Mesh(boxGeometry1, boxMaterial1);
+boxMesh1.position.set(2, 0, 0);
+
+const boxGeometry2 = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial2 = new THREE.MeshBasicMaterial({ color: "blue" });
+const boxMesh2 = new THREE.Mesh(boxGeometry2, boxMaterial2);
+boxMesh2.position.set(-2, 0, 0);
+group.add(boxMesh1);
+group.add(boxMesh2);
+
+group.position.set(0.5, 0, 0, 0);
+group.scale.y = 2;
+// group.rotation.reorder("YXZ");
+group.rotation.set(Math.PI / 3, Math.PI, 0);
+
+scene.add(group);
+console.log(group.position.length());
+
+// const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const mesh = new THREE.Mesh(boxGeometry, material);
+// scene.add(mesh);
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -20,6 +50,7 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 camera.position.z = 3;
+// camera.lookAt(boxMesh1.position);
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({
