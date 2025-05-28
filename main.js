8b67452a28e0d5116e09eaf11ed4cacef3a34b0e
zane <zanesdf@qq.com>
2025-05-28 22:40:56 +0800

    第五课 动画 requestAnimationFrame gsap

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { color } from "three/tsl";
+import gsap from "gsap";
 
 const Canvas = document.querySelector("canvas.webgl");
 
@@ -12,36 +12,32 @@ const size = {
 const axesHelper = new THREE.AxesHelper(2);
 scene.add(axesHelper);
 
-const group = new THREE.Group();
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const mesh = new THREE.Mesh(boxGeometry, material);
-group.add(mesh);
-
-const boxGeometry1 = new THREE.BoxGeometry(1, 1, 1);
-const boxMaterial1 = new THREE.MeshBasicMaterial({ color: "red" });
-const boxMesh1 = new THREE.Mesh(boxGeometry1, boxMaterial1);
-boxMesh1.position.set(2, 0, 0);
-
-const boxGeometry2 = new THREE.BoxGeometry(1, 1, 1);
-const boxMaterial2 = new THREE.MeshBasicMaterial({ color: "blue" });
-const boxMesh2 = new THREE.Mesh(boxGeometry2, boxMaterial2);
-boxMesh2.position.set(-2, 0, 0);
-group.add(boxMesh1);
-group.add(boxMesh2);
-
-group.position.set(0.5, 0, 0, 0);
-group.scale.y = 2;
-// group.rotation.reorder("YXZ");
-group.rotation.set(Math.PI / 3, Math.PI, 0);
-
-scene.add(group);
-console.log(group.position.length());
-
+// const group = new THREE.Group();
 // const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 // const mesh = new THREE.Mesh(boxGeometry, material);
-// scene.add(mesh);
+// group.add(mesh);
+
+// const boxGeometry1 = new THREE.BoxGeometry(1, 1, 1);
+// const boxMaterial1 = new THREE.MeshBasicMaterial({ color: "red" });
+// const boxMesh1 = new THREE.Mesh(boxGeometry1, boxMaterial1);
+// boxMesh1.position.set(2, 0, 0);
+// const boxGeometry2 = new THREE.BoxGeometry(1, 1, 1);
+// const boxMaterial2 = new THREE.MeshBasicMaterial({ color: "blue" });
+// const boxMesh2 = new THREE.Mesh(boxGeometry2, boxMaterial2);
+// boxMesh2.position.set(-2, 0, 0);
+// group.add(boxMesh1);
+// group.add(boxMesh2);
+// group.position.set(0.5, 0, 0, 0);
+// group.scale.y = 2;
+// group.rotation.reorder("YXZ");
+// group.rotation.set(Math.PI / 3, Math.PI, 0);
+// scene.add(group);
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const mesh = new THREE.Mesh(boxGeometry, material);
+scene.add(mesh);
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -58,3 +54,20 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(size.with, size.height);
 renderer.render(scene, camera);
+
+// const clock = new THREE.Clock()
+gsap.to(mesh.position, { x: 2, delay: 1, direction: 1 });
+gsap.to(mesh.position, { x: 0, delay: 2, direction: 1 });
+
+const tick = () => {
+  // const time = clock.getElapsedTime()
+  // console.log(time)
+  // mesh.rotation.z = time
+  // mesh.position.x = time
+  // mesh.position.y = Math.cos(time)
+
+  renderer.render(scene, camera);
+  requestAnimationFrame(tick);
+};
+
+tick();
