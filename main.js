9b54bd0200fad90322c61430a23c7f79758b5a64
zane <zanesdf@qq.com>
2025-06-11 22:36:10 +0800

    第11课 各种材质

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 import * as THREE from "three";
 import gsap from "gsap";
+import { RGBELoader } from "three/examples/jsm/Addons.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import GUI from "lil-gui";
+const debugGUI = new GUI({
+  title: "debug gui",
+  width: 300,
+});
+const boxGui = debugGUI.addFolder("Box");
 
 const TexturesManage = new THREE.LoadingManager();
 TexturesManage.onStart = () => {
@@ -10,34 +16,6 @@ TexturesManage.onStart = () => {
 TexturesManage.onError = () => {
   console.log("onError");
 };
-const TexturesLoader = new THREE.TextureLoader(TexturesManage);
-const color = TexturesLoader.load("/textures/door/color.jpg");
-// const color = TexturesLoader.load("/textures/checkerboard-8x8.png");
-// const color = TexturesLoader.load("/textures/checkerboard-1024x1024.png");
-
-// color.repeat.x = 2;
-// color.repeat.y = 3;
-// color.wrapS = THREE.RepeatWrapping;
-// color.wrapS = THREE.MirroredRepeatWrapping;
-// color.wrapT = THREE.RepeatWrapping;
-// color.wrapS = THREE.MirroredRepeatWrapping;
-
-// color.offset.x = 0.5;
-// color.offset.y = 0.5;
-
-// color.rotation = Math.PI / 4;
-// color.center.x = 0.5;
-// color.center.y = 0.5;
-
-color.magFilter = THREE.NearestFilter;
-
-const GlobalConfig = {};
-
-const debugGUI = new GUI({
-  title: "debug gui",
-  width: 300,
-});
-const boxGui = debugGUI.addFolder("Box");
 
 const Canvas = document.querySelector("canvas.webgl");
 
@@ -72,11 +50,10 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.z = 0.7;
-camera.position.y = 0.7;
-camera.position.x = 0.7;
+camera.position.z = 2.5;
+camera.position.y = 0;
+camera.position.x = 0;
 scene.add(camera);
-// camera.lookAt(boxMesh1.position);
 renderer.setSize(size.with, size.height);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.render(scene, camera);
@@ -87,70 +64,115 @@ scene.add(axesHelper);
 const control = new OrbitControls(camera, Canvas);
 control.enableDamping = true;
 
-GlobalConfig.color = "#0edd1c";
-const BoxGeometry = new THREE.BoxGeometry(1, 1, 1, 1, 1, 1);
-const BoxMaterial = new THREE.MeshBasicMaterial({
-  // color: GlobalConfig.color,
-  map: color,
-});
-const mesh = new THREE.Mesh(BoxGeometry, BoxMaterial);
-scene.add(mesh);
+const TexturesLoader = new THREE.TextureLoader(TexturesManage);
+const alpha = TexturesLoader.load("/textures/door/alpha.jpg");
+const ambientOcclusion = TexturesLoader.load(
+  "/textures/door/ambientOcclusion.jpg"
+);
+const color = TexturesLoader.load("/textures/door/color.jpg");
+const height = TexturesLoader.load("/textures/door/height.jpg");
+const metalness = TexturesLoader.load("/textures/door/metalness.jpg");
+const normal = TexturesLoader.load("/textures/door/normal.jpg");
+const roughness = TexturesLoader.load("/textures/door/roughness.jpg");
+const gradients_3 = TexturesLoader.load("/textures/gradients/3.jpg");
+const gradients_5 = TexturesLoader.load("/textures/gradients/5.jpg");
+const matcaps = TexturesLoader.load("/textures/matcaps/5.png");
 
-boxGui.add(mesh.position, "z", 0, 2, 0.1);
-boxGui.add(BoxMaterial, "wireframe");
-boxGui.addColor(GlobalConfig, "color").onChange((val) => {
-  BoxMaterial.color.set(val);
-  console.log(val);
-});
-GlobalConfig.meshAnimate = () => {
-  gsap.to(mesh.position, { x: 2 });
-  gsap.to(mesh.rotation, { x: mesh.rotation.x + Math.PI * 2 });
-};
-boxGui.add(GlobalConfig, "meshAnimate");
+// color.colorSpace = THREE.SRGBColorSpace;
+// const material = new THREE.MeshBasicMaterial();
+// material.map = color;
+// material.transparent = true;
+// material.opacity = 0.1;
+// material.side = THREE.DoubleSide;
 
-GlobalConfig.widthSegments = 1;
-GlobalConfig.heightSegments = 1;
-GlobalConfig.depthSegments = 1;
+// 越近越亮
+// const material = new THREE.MeshDepthMaterial();
+// material.map = color;
 
-boxGui.add(GlobalConfig, "widthSegments", 1, 4, 1).onFinishChange((val) => {
-  mesh.geometry.dispose();
-  GlobalConfig.widthSegments = val;
-  mesh.geometry = new THREE.BoxGeometry(
-    1,
-    1,
-    1,
-    val,
-    GlobalConfig.heightSegments,
-    GlobalConfig.depthSegments
-  );
-});
-boxGui.add(GlobalConfig, "heightSegments", 1, 4, 1).onFinishChange((val) => {
-  mesh.geometry.dispose();
-  GlobalConfig.heightSegments = val;
-  mesh.geometry = new THREE.BoxGeometry(
-    1,
-    1,
-    1,
-    GlobalConfig.widthSegments,
-    val,
-    GlobalConfig.depthSegments
-  );
-});
-boxGui.add(GlobalConfig, "depthSegments", 1, 4, 1).onFinishChange((val) => {
-  mesh.geometry.dispose();
-  GlobalConfig.depthSegments = val;
-  mesh.geometry = new THREE.BoxGeometry(
-    1,
-    1,
-    1,
-    GlobalConfig.widthSegments,
-    GlobalConfig.heightSegments,
-    val
-  );
-});
-boxGui.close();
+// const material = new THREE.MeshNormalMaterial();
+// material.flatShading = true;
 
-// boxGui.addFolder("boxItem");
+// const material = new THREE.MeshMatcapMaterial();
+// material.matcap = matcaps; // 网格捕捉
+
+// const material = new THREE.MeshLambertMaterial();
+
+// const material = new THREE.MeshPhongMaterial();
+// material.shininess = 10000;
+// material.specular = new THREE.Color("#ff0000");
+
+// const material = new THREE.MeshToonMaterial();
+// gradients_5.magFilter = THREE.NearestFilter;
+// material.gradientMap = gradients_5;
+
+// const material = new THREE.MeshStandardMaterial();
+// // 对灯光反应
+// material.metalness = 1;
+// material.roughness = 1;
+// boxGui.add(material, "metalness").min(0).max(1).step(0.0001);
+// boxGui.add(material, "roughness").min(0).max(1).step(0.0001);
+// material.map = color;
+// material.aoMap = ambientOcclusion;
+// material.aoMapIntensity = 1;
+// // material.displacementMap = height;
+// material.displacementScale = 0.2;
+// boxGui.add(material, "displacementScale").min(0).max(1).step(0.001);
+// material.metalnessMap = metalness;
+// material.roughnessMap = roughness;
+// material.normalMap = normal;
+// material.normalScale.set(0.5, 0.5);
+// material.transparent = true;
+// material.alphaMap = alpha;
+
+const material = new THREE.MeshPhysicalMaterial();
+// // 对灯光反应
+material.metalness = 1;
+material.roughness = 1;
+boxGui.add(material, "metalness").min(0).max(1).step(0.0001);
+boxGui.add(material, "roughness").min(0).max(1).step(0.0001);
+material.map = color;
+material.aoMap = ambientOcclusion;
+material.aoMapIntensity = 1;
+// material.displacementMap = height;
+material.displacementScale = 0.2;
+boxGui.add(material, "displacementScale").min(0).max(1).step(0.001);
+material.metalnessMap = metalness;
+material.roughnessMap = roughness;
+material.normalMap = normal;
+material.normalScale.set(0.5, 0.5);
+material.transparent = true;
+material.alphaMap = alpha;
+material.clearcoat = 1;
+material.clearcoatRoughness = 0;
+
+const planeGeometry = new THREE.PlaneGeometry(1.6, 1.6, 20, 20);
+const sphereGeometry = new THREE.SphereGeometry(0.8, 16, 16);
+
+const plane = new THREE.Mesh(planeGeometry, material);
+plane.position.x = 1;
+const sphere = new THREE.Mesh(sphereGeometry, material);
+sphere.position.x = -1.5;
+
+scene.add(plane, sphere);
+
+const light = new THREE.PointLight("#ffffff", 30);
+light.position.x = 2;
+light.position.y = 2;
+light.position.z = 2;
+scene.add(light);
+
+const ambientLight = new THREE.AmbientLight("#ffffff", 0.5);
+scene.add(ambientLight);
+
+const rgbeLoader = new RGBELoader();
+rgbeLoader.load("/textures/environmentMap/2k.hdr", (environmentMap) => {
+  console.log("加载完毕");
+  environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+  scene.background = environmentMap;
+  scene.environment = environmentMap;
+});
+
+
 
 const tick = () => {
   control.update();
